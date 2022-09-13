7a1c1a33e3a74c785e645729187ab8515ace2089
zhanglongxia <zhanglongxia@google.com>
2022-09-14 00:48:38 +0800

    Add Android.bp, format the source code

--- spidev_test.c
+++ spidev_test.c
@@ -11,239 +11,229 @@
  * Cross-compile with cross-gcc -I/path/to/cross-kernel/include
  */
 
+#include <assert.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <linux/spi/spidev.h>
+#include <linux/types.h>
 #include <stdint.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <getopt.h>
-#include <fcntl.h>
 #include <sys/ioctl.h>
-#include <linux/types.h>
-#include <linux/spi/spidev.h>
+#include <unistd.h>
 
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+#define BUF_MAX_SIZE 256
 
-
-static const char *device = "/dev/spidev1.1";
-static uint8_t mode;
-static uint8_t bits = 8;
-static uint32_t speed = 500000;
-static uint16_t delay;
-static uint8_t size;
-static uint8_t *pbuffer = NULL;
-
-
+static const char *s_device   = "/dev/spidev1.0";
+static uint8_t     s_mode     = 0;
+static uint8_t     s_bits     = 8;
+static uint32_t    s_speed    = 1000000;
+static uint16_t    s_delay_us = 20;
+static uint8_t     s_size;
+static uint8_t     s_tx_buf[BUF_MAX_SIZE];
+static uint8_t     s_rx_buf[BUF_MAX_SIZE];
 
 static void print_usage(const char *prog)
 {
-  printf("Usage: %s [-DsbdlHOLC3] [X] \n", prog ? prog : "");
-  puts("  -D --device   device to use (default /dev/spidev1.1)\n"
-       "  -s --speed    max speed (Hz)\n"
-       "  -d --delay    delay (usec)\n"
-       "  -b --bpw      bits per word \n"
-       "  -l --loop     loopback\n"
-       "  -H --cpha     clock phase\n"
-       "  -O --cpol     clock polarity\n"
-       "  -L --lsb      least significant bit first\n"
-       "  -C --cs-high  chip select active high\n"
-       "  -3 --3wire    SI/SO signals shared\n"
-       "  -X --xdata    hexadecimal data\n" 
-       "./spidev_test -D /dev/spidev1.1 -s 26000000 -b 8 -X 0xbb 0xcc\n");
-  if(prog)
-    exit(1);
+    printf("Usage: %s [-DsbdlHOLC3] [X] \n", prog ? prog : "");
+    printf("  -D --device   device to use (default /dev/spidev1.1)\n"
+           "  -s --speed    max speed (Hz)\n"
+           "  -d --delay    delay (usec)\n"
+           "  -b --bpw      s_bits per word \n"
+           "  -l --loop     loopback\n"
+           "  -H --cpha     clock phase\n"
+           "  -O --cpol     clock polarity\n"
+           "  -L --lsb      least significant bit first\n"
+           "  -C --cs-high  chip select active high\n"
+           "  -3 --3wire    SI/SO signals shared\n"
+           "  -X --xdata    hexadecimal data\n"
+           "./spidev_test -D /dev/spidev1.0 -s 1000000 -b 8 -X 0xaa 0xbb 0xcc\n");
+
+    if (prog)
+    {
+        exit(EXIT_FAILURE);
+    }
 }
 
 static void pabort(const char *s)
 {
-  if(pbuffer != NULL)
-    free(pbuffer);
-  pbuffer = NULL;
-  print_usage(NULL);
-  perror(s);
-  abort();
+    print_usage(NULL);
+    perror(s);
+    abort();
 }
 
-
-static void transfer(int fd, uint8_t * tx)
+static void transfer(int fd)
 {
-  int ret;
+    int i;
 
-  uint8_t *rx;
-  rx = malloc(size);
-  struct spi_ioc_transfer tr = {
-    .tx_buf = (unsigned long)tx,
-    .rx_buf = (unsigned long)rx,
-    .len = ARRAY_SIZE(tx),
-    .delay_usecs = delay,
-    .speed_hz = speed,
-    .bits_per_word = bits,
-  };
+    struct spi_ioc_transfer transfer = {
+        .tx_buf        = (unsigned long)s_tx_buf,
+        .rx_buf        = (unsigned long)s_rx_buf,
+        .len           = s_size,
+        .delay_usecs   = s_delay_us,
+        .speed_hz      = s_speed,
+        .bits_per_word = s_bits,
+    };
 
-  ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-  if(ret < 1)
-    pabort("can't send spi message");
+    if (ioctl(fd, SPI_IOC_MESSAGE(1), &transfer) < 1)
+    {
+        pabort("Failed to send spi message");
+    }
 
-  printf("TX: ");
-  for(ret = 0; ret < size; ret++)
-  {
-    if(!(ret % 6) && ret)
-      puts("");
-    printf("%.2x ", tx[ret]);
-  }
+    printf("TX: ");
+    for (i = 0; i < s_size; i++)
+    {
+        printf("%.2x ", s_tx_buf[i]);
+    }
+    printf("\r\n");
 
-  printf("\t RX: ");
-  for(ret = 0; ret < size; ret++)
-  {
-    if(!(ret % 6) && ret)
-      puts("");
-    printf("%.2x ", rx[ret]);
-  }
-  puts("");
+    printf("RX: ");
+    for (i = 0; i < s_size; i++)
+    {
+        printf("%.2x ", s_rx_buf[i]);
+    }
+    printf("\r\n");
 }
 
 static void parse_opts(int argc, char *argv[])
 {
-  int i, index;
-  char *p;
+    int   i, index;
+    char *p;
 
-  while(1)
-  {
-    static const struct option lopts[] = {
-      {"device", 1, 0, 'D'},
-      {"speed", 1, 0, 's'},
-      {"delay", 1, 0, 'd'},
-      {"bpw", 1, 0, 'b'},
-      {"loop", 0, 0, 'l'},
-      {"cpha", 0, 0, 'H'},
-      {"cpol", 0, 0, 'O'},
-      {"lsb", 0, 0, 'L'},
-      {"cs-high", 0, 0, 'C'},
-      {"3wire", 0, 0, '3'},
-      {"no-cs", 0, 0, 'N'},
-      {"ready", 0, 0, 'R'},
-      {"Xdata", 1, 0, 'X'},
-      {NULL, 0, 0, 0},
-    };
-    int c;
-
-    c = getopt_long(argc, argv, "D:s:d:b:lHOLC3NRX", lopts, NULL);
-
-    if(c == -1)
-      break;
-
-    switch (c)
+    while (1)
     {
-      case 'D':
-        device = optarg;
-        break;
-      case 's':
-        speed = atoi(optarg);
-        break;
-      case 'd':
-        delay = atoi(optarg);
-        break;
-      case 'b':
-        bits = atoi(optarg);
-        break;
-      case 'l':
-        mode |= SPI_LOOP;
-        break;
-      case 'H':
-        mode |= SPI_CPHA;
-        break;
-      case 'O':
-        mode |= SPI_CPOL;
-        break;
-      case 'L':
-        mode |= SPI_LSB_FIRST;
-        break;
-      case 'C':
-        mode |= SPI_CS_HIGH;
-        break;
-      case '3':
-        mode |= SPI_3WIRE;
-        break;
-#if 0
-      case 'N':
-        mode |= SPI_NO_CS;
-        break;
-      case 'R':
-        mode |= SPI_READY;
-        break;
-#endif
-      case 'X':
-        i = 0;
-        size = argc - optind;
-        pbuffer = malloc(size);
-        for(index = optind; index < argc; index++, i++)
+        int                        c;
+        static const struct option opts[] = {
+            {"device", 1, 0, 'D'},  {"speed", 1, 0, 's'}, {"delay", 1, 0, 'd'}, {"bpw", 1, 0, 'b'},
+            {"loop", 0, 0, 'l'},    {"cpha", 0, 0, 'H'},  {"cpol", 0, 0, 'O'},  {"lsb", 0, 0, 'L'},
+            {"cs-high", 0, 0, 'C'}, {"3wire", 0, 0, '3'}, {"no-cs", 0, 0, 'N'}, {"ready", 0, 0, 'R'},
+            {"Xdata", 1, 0, 'X'},   {NULL, 0, 0, 0},
+        };
+
+        c = getopt_long(argc, argv, "D:s:d:b:lHOLC3NRX", opts, NULL);
+        if (c == -1)
         {
-          pbuffer[i] = strtol(argv[index], &p, 0);
+            break;
         }
-        break;
-      default:
-        print_usage(argv[0]);
-        break;
+
+        switch (c)
+        {
+        case 'D':
+            s_device = optarg;
+            break;
+        case 's':
+            s_speed = (uint32_t)atoi(optarg);
+            break;
+        case 'd':
+            s_delay_us = (uint16_t)atoi(optarg);
+            break;
+        case 'b':
+            s_bits = (uint8_t)atoi(optarg);
+            break;
+        case 'l':
+            s_mode |= SPI_LOOP;
+            break;
+        case 'H':
+            s_mode |= SPI_CPHA;
+            break;
+        case 'O':
+            s_mode |= SPI_CPOL;
+            break;
+        case 'L':
+            s_mode |= SPI_LSB_FIRST;
+            break;
+        case 'C':
+            s_mode |= SPI_CS_HIGH;
+            break;
+        case '3':
+            s_mode |= SPI_3WIRE;
+            break;
+        case 'N':
+            s_mode |= SPI_NO_CS;
+            break;
+        case 'R':
+            s_mode |= SPI_READY;
+            break;
+        case 'X':
+            i      = 0;
+            s_size = argc - optind;
+
+            if (s_size > BUF_MAX_SIZE)
+            {
+                printf("The hex data is too long");
+                exit(EXIT_FAILURE);
+            }
+
+            for (index = optind; index < argc; index++, i++)
+            {
+                s_tx_buf[i] = (uint8_t)strtol(argv[index], &p, 0);
+            }
+            break;
+        default:
+            print_usage(argv[0]);
+            break;
+        }
     }
-  }
 }
 
 int main(int argc, char *argv[])
 {
-  int ret = 0;
-  int fd;
+    int fd;
 
-  pbuffer = NULL;
+    parse_opts(argc, argv);
 
-  parse_opts(argc, argv);
+    fd = open(s_device, O_RDWR);
+    if (fd < 0)
+    {
+        pabort("Failed open SPI device");
+    }
 
-  fd = open(device, O_RDWR);
-  if(fd < 0)
-    pabort("can't open device");
+    /*
+     * spi mode
+     */
+    if (ioctl(fd, SPI_IOC_WR_MODE, &s_mode) == -1)
+    {
+        pabort("Failed to set spi mode");
+    }
 
-  /*
-   * spi mode
-   */
-  ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
-  if(ret == -1)
-    pabort("can't set spi mode");
+    if (ioctl(fd, SPI_IOC_RD_MODE, &s_mode) == -1)
+    {
+        pabort("Failed to get spi mode");
+    }
 
-  ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
-  if(ret == -1)
-    pabort("can't get spi mode");
+    /*
+     * bits per word
+     */
+    if (ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &s_bits) == -1)
+    {
+        pabort("Failed to set bits per word");
+    }
 
-  /*
-   * bits per word
-   */
-  ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
-  if(ret == -1)
-    pabort("can't set bits per word");
+    if (ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &s_bits) == -1)
+    {
+        pabort("Failed to get bits per word");
+    }
 
-  ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
-  if(ret == -1)
-    pabort("can't get bits per word");
+    /*
+     * max speed hz
+     */
+    if (ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &s_speed) == -1)
+    {
+        pabort("Failed to set max speed hz");
+    }
 
-  /*
-   * max speed hz
-   */
-  ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
-  if(ret == -1)
-    pabort("can't set max speed hz");
+    if (ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &s_speed) == -1)
+    {
+        pabort("Failed to get max speed hz");
+    }
 
-  ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
-  if(ret == -1)
-    pabort("can't get max speed hz");
+    printf("spi mode: %d\n", s_mode);
+    printf("bits per word: %d\n", s_bits);
+    printf("max speed: %d Hz (%d KHz)\n", s_speed, s_speed / 1000);
 
-  printf("spi mode: %d\n", mode);
-  printf("bits per word: %d\n", bits);
-  printf("max speed: %d Hz (%d KHz)\n", speed, speed / 1000);
+    transfer(fd);
 
-  transfer(fd, pbuffer);
+    close(fd);
 
-  if(pbuffer != NULL)
-    free(pbuffer);
-  pbuffer = NULL;
-
-  close(fd);
-
-  return ret;
+    return 0;
 }
